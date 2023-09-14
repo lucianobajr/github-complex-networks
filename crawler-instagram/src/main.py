e268d8ed6fffa7a78891078230bd57daa13023e2
lucianobajr <luciano.alcantara@ufv.br>
2023-09-14 13:48:24 -0300

    add

--- crawler-instagram/src/main.py
+++ crawler-instagram/src/main.py
@@ -3,18 +3,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
-from config.environments import USERNAME_INSTAGRAM,PASSWORD_INSTAGRAM
+import time
+
+from config.environments import USERNAME_INSTAGRAM, PASSWORD_INSTAGRAM
 
 browser = webdriver.Chrome("./resources/chromedriver")
 
 browser.get("http://instagram.com")
 
-username = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-password = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+username = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
 
 username.send_keys(USERNAME_INSTAGRAM)
 password.send_keys(PASSWORD_INSTAGRAM)
 
-submit = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+time.sleep(5)
+submit = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
 
 submit.click()
+
+time.sleep(5)
+try:
+    time.sleep(5)
+    button = browser.find_element(By.CSS_SELECTOR, "div.x1i10hfl")
+    button.click()
+except:
+    time.sleep(1)
+
+
+# Turn Notifications Off
+time.sleep(5)
+
+try:
+    button = browser.find_element(
+        By.CSS_SELECTOR, "button._a9--:nth-child(2)")
+    button.click()
+except:
+    time.sleep(1)
